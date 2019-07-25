f63e20644fe60029f104126c601b90fc03190778
Tommy Clark <tommy.clark@sainsburys.co.uk>
2019-07-25 22:24:56 +0100

    Use README.md as PyPI description.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-import sys
 from setuptools import setup, find_packages
+
+# read the contents of the README file for PyPI
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup (
     name="asciigit",
-    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.0.2",
     packages=find_packages(),
     tests_require=['mock'],
     install_requires=[
